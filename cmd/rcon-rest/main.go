e272d623ac1869bcd8b52bf9944ebc535d78b970
Serena Tiede <serena.tiede+github@gmail.com>
2020-03-28 19:42:49 -0500

    finish api spec

--- cmd/rcon-rest/main.go
+++ cmd/rcon-rest/main.go
@@ -1,27 +1,5 @@
 package main
 
-import (
-	"fmt"
-	mcNet "github.com/Tnze/go-mc/net"
-	"go.uber.org/zap"
-)
-
 func main() {
-	connection, err := mcNet.DialRCON("127.0.0.1:25575", "T4Yk7QF2wV9kjdwcQfZc2CmwxLkInxfL")
-	if err != nil {
-		zap.Error(err)
-		return
-	}
-	commandErr := connection.Cmd("kick boredOfEnnui")
-	if commandErr != nil {
-		zap.Error(commandErr)
-		return
-	}
-	response, responseErr := connection.Resp()
-	if responseErr != nil {
-		zap.Error(responseErr)
-		return
-	}
 
-	fmt.Println(response)
 }
